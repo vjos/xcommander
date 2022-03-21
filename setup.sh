49e14dd3d3e092f7d4a83c5310cc72fa4e84fe7b
jventers <jventers@protonmail.com>
2022-03-21 19:20:47 +0000

    make setup work with elevated permissions

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 cp egpu-primary-igpu-offload.conf /etc/X11/
 
 # add to .bash_profile, uses tty1 and tty2 for xcom by default
-cat >> ~/.bash_profile << EOM
+usr=$(logname)
+cat >> $usr/.bash_profile << EOM
 
 # startx on tty 1/2
 tty=\$(fgconsole 2>/dev/null)
